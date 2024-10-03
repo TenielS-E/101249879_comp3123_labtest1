b34a050da8ef04d982a1da4b74a4b06a16941c7e
Teniel Smith-Edwards <tsmith1227@outlook.com>
2024-10-03 17:40:42 -0400

    question 1 solution

---
+++ question1/index.js
@@ -0,0 +1,27 @@
+/*
+Create a script with a function named lowerCaseWords that takes a
+mixed array as input.
+The function will do the following.
+o return a promise that is resolved or rejected
+o filter the non-strings and lower case the remaining words
+*/
+
+// Function created:
+function lowerCaseWords(arr){
+    // declare variable
+    var strArr = [];
+
+    // returns promise resolved or rejected
+    if(strArr = arr.filter(e => typeof e === "string")){
+        console.log(strArr);
+        return Promise.resolve(strArr);
+    } else {
+        return Promise.reject("There is no string in the array.")
+    }
+}
+
+// Test Array:
+const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings'];
+
+// Callback Function with parameter
+lowerCaseWords(mixedArray);
